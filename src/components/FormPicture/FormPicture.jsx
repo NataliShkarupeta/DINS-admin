607b5a01cc2331ac59eb0405ef1605b0375e5e70
Nataliia Shkarupeta <natalinardi.kh@gmail.com>
2023-04-25 16:17:33 +0300

    creat objects for db

--- src/components/FormPicture/FormPicture.jsx
+++ src/components/FormPicture/FormPicture.jsx
@@ -7,6 +7,7 @@ import {
   FileInput,
   TitleBlock,
 } from "../styles.styled";
+import { v4 as uuidv4 } from "uuid";
 
 export const FormPicture = () => {
   const [file, setFile] = useState("");
@@ -14,18 +15,12 @@ export const FormPicture = () => {
     <div>Please select an Image for Preview</div>
   );
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // TODO: do something with -> file
-    console.log("handle uploading-", file);
-  };
-
   const handleImageChange = (e) => {
     e.preventDefault();
 
     let reader = new FileReader();
-    let file = e.target.files[0];
 
+    let file = e.target.files[0];
     reader.onloadend = () => {
       setFile(file);
       setImagePreviewUrl(reader.result);
@@ -33,19 +28,37 @@ export const FormPicture = () => {
 
     reader.readAsDataURL(file);
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const title = formData.get("title1");
+    const descriptions = formData.get("descriptions2");
+
+    const newPicture = {
+      title,
+      descriptions,
+      file: { ...file, imagePreviewUrl: imagePreviewUrl },
+      id: uuidv4(),
+      dateStamp: Date.now(),
+    };
+
+    console.log(newPicture);
+    e.target.reset();
+  };
 
   return (
     <>
-      <TitleBlock> Завантажте картину</TitleBlock>
+      <TitleBlock> Завантаж картину</TitleBlock>
       <Form onSubmit={handleSubmit}>
         <label htmlFor="">
           Title
-          <input type="text" name="title" />
+          <input type="text" name="title1" />
         </label>
         <Wrap>
           <label htmlFor="">
             Descriptions
-            <FileInput type="text" name="descriptions"></FileInput>
+            <FileInput type="text" name="descriptions2"></FileInput>
           </label>
           <label htmlFor="">
             File
@@ -60,9 +73,7 @@ export const FormPicture = () => {
           </label>
         </Wrap>
 
-        <SubmitButton onClick={(e) => handleSubmit(e)}>
-          Upload picture
-        </SubmitButton>
+        <SubmitButton type="submit">Upload picture</SubmitButton>
       </Form>
     </>
   );
--- src/components/FormPost/FormPost.jsx
+++ src/components/FormPost/FormPost.jsx
@@ -6,25 +6,56 @@ import {
   Wrap,
   Label,
 } from "../styles.styled";
+import { v4 as uuidv4 } from "uuid";
 
 export const FormPost = () => {
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const title = formData.get("title");
+    const descriptions = formData.get("descriptions");
+    const titleEn = formData.get("titleEn");
+    const descriptionsEn = formData.get("descriptionsEn");
+
+    const newPost = {
+      title,
+      descriptions,
+      titleEn,
+      descriptionsEn,
+      id: uuidv4(),
+      dateStamp: Date.now(),
+    };
+  
+
+    console.log("newPost=>", newPost);
+    e.target.reset();
+  };
+
   return (
     <>
-      <TitleBlock> Створи новий пост</TitleBlock>
-      <Form>
+      <TitleBlock>Створи новий пост</TitleBlock>
+      <Form onSubmit={handelSubmit}>
         <Wrap>
           <Label htmlFor="">
             <span>Title</span>
-
-            <input type="text" />
+            <input type="text" name="title" />
           </Label>
           <label htmlFor="">
             Descriptions
-            <FileInput type="text" />
+            <FileInput type="text" name="descriptions" />
           </label>
         </Wrap>
-
-        <SubmitButton>Upload Post</SubmitButton>
+        <Wrap>
+          <Label htmlFor="">
+            <span>TitleEn</span>
+            <input type="text" name="titleEn" />
+          </Label>
+          <label htmlFor="">
+            DescriptionsEn
+            <FileInput type="text" name="descriptionsEn" />
+          </label>
+        </Wrap>
+        <SubmitButton type="submit">Upload Post</SubmitButton>
       </Form>
     </>
   );
